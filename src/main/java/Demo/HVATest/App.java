202293181b4a43d6b02ad6f55c9ac38c49c6e6a1
badheypratik <badheypratik@gmail.com>
2023-07-24 10:01:22 +0530

    first round of commit...

---
+++ src/main/java/Demo/HVATest/App.java
@@ -0,0 +1,13 @@
+package Demo.HVATest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{    
+    public static void Test()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Demo/HVATest/AppTest.java
@@ -0,0 +1,22 @@
+package Demo.HVATest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	App HVA = new App();
+    	HVA.Test();
+        assertTrue( true );
+    }
+}
